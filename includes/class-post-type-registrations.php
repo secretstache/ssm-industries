9449e266c9c9638b5fb6afdb90a2b9031302e64b
Rich Staats <rich@secretstache.com>
2017-04-12 15:21:38 -0600

    initial commit

---
+++ includes/class-post-type-registrations.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * SSM Industries
+ *
+ * @package   SSM_Industries
+ * @license   GPL-2.0+
+ */
+
+/**
+ * Register post types and taxonomies.
+ *
+ * @package SSM_Industries
+ */
+class SSM_Industries_Registrations {
+
+	public $post_type = 'industry';
+
+	public function init() {
+		// Add the SSM Industries and taxonomies
+		add_action( 'init', array( $this, 'register' ) );
+	}
+
+	/**
+	 * Initiate registrations of post type and taxonomies.
+	 *
+	 * @uses SSM_Industries_Registrations::register_post_type()
+	 * @uses SSM_Industries_Registrations::register_taxonomy_category()
+	 */
+	public function register() {
+		$this->register_post_type();
+	}
+
+	/**
+	 * Register the custom post type.
+	 *
+	 * @link http://codex.wordpress.org/Function_Reference/register_post_type
+	 */
+	protected function register_post_type() {
+		
+		$labels = array(
+			'name'                  => _x( 'Industries', 'Post Type General Name', 'ssm' ),
+			'singular_name'         => _x( 'Industry', 'Post Type Singular Name', 'ssm' ),
+			'menu_name'             => __( 'Industries', 'ssm' ),
+			'name_admin_bar'        => __( 'Industry', 'ssm' ),
+			'archives'              => __( 'Industry Archives', 'ssm' ),
+			'attributes'            => __( 'Industry Attributes', 'ssm' ),
+			'parent_item_colon'     => __( 'Parent Industry:', 'ssm' ),
+			'all_items'             => __( 'Industries', 'ssm' ),
+			'add_new_item'          => __( 'Add New Industry', 'ssm' ),
+			'add_new'               => __( 'Add New', 'ssm' ),
+			'new_item'              => __( 'New Industry', 'ssm' ),
+			'edit_item'             => __( 'Edit Industry', 'ssm' ),
+			'update_item'           => __( 'Update Industry', 'ssm' ),
+			'view_item'             => __( 'View Industry', 'ssm' ),
+			'view_items'            => __( 'View Industries', 'ssm' ),
+			'search_items'          => __( 'Search Industry', 'ssm' ),
+			'not_found'             => __( 'No industries found', 'ssm' ),
+			'not_found_in_trash'    => __( 'No industries found in Trash', 'ssm' ),
+			'featured_image'        => __( 'Industry Icon', 'ssm' ),
+			'set_featured_image'    => __( 'Set icon', 'ssm' ),
+			'remove_featured_image' => __( 'Remove icon', 'ssm' ),
+			'use_featured_image'    => __( 'Use as icon', 'ssm' ),
+			'insert_into_item'      => __( 'Insert into Industry', 'ssm' ),
+			'uploaded_to_this_item' => __( 'Uploaded to this Industry', 'ssm' ),
+			'items_list'            => __( 'Industry List', 'ssm' ),
+			'items_list_navigation' => __( 'Industry list navigation', 'ssm' ),
+			'filter_items_list'     => __( 'Filter Industry list', 'ssm' ),
+		);
+		
+		$args = array(
+			'label'                 => __( 'Industry', 'ssm' ),
+			'labels'                => $labels,
+			'supports'              => array( 'title', 'thumbnail', 'editor' ),
+			'hierarchical'          => false,
+			'public'                => false,
+			'show_ui'               => true,
+			'show_in_menu'          => true,
+			'menu_position'         => 5,
+			'menu_icon'             => 'dashicons-businessman',
+			'show_in_admin_bar'     => true,
+			'show_in_nav_menus'     => false,
+			'can_export'            => true,
+			'has_archive'           => false,		
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => true,
+			'capability_type'       => 'page',
+		);
+
+		$args = apply_filters( 'ssm_industries_args', $args );
+
+		register_post_type( $this->post_type, $args );
+
+	}
+	
+
+}
---
+++ ssm-industries.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * SSM Industries
+ *
+ * @package   SSM_Industries
+ * @license   GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: SSM Industries
+ * Plugin URI:  http://secretstache.com
+ * Description: Enables an Industry Custom Post Type.
+ * Version:     0.1.0
+ * Author:      Secret Stache Media
+ * Author URI:  http://secretstache.com
+ * Text Domain: ssm-industries
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path: /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Required files for registering the post type and taxonomies.
+require plugin_dir_path( __FILE__ ) . 'includes/class-post-type.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-post-type-registrations.php';
+
+// Instantiate registration class, so we can add it as a dependency to main plugin class.
+$post_type_registrations = new SSM_Industries_Registrations;
+
+// Instantiate main plugin file, so activation callback does not need to be static.
+$post_type = new SSM_Industries( $post_type_registrations );
+
+// Register callback that is fired when the plugin is activated.
+register_activation_hook( __FILE__, array( $post_type, 'activate' ) );
+
+// Initialize registrations for post-activation requests.
+$post_type_registrations->init();
+
+/**
+ * Adds styling to the dashboard for the post type and adds Project posts
+ * to the "At a Glance" metabox.
+ */
+if ( is_admin() ) {
+
+	// Loads for users viewing the WordPress dashboard
+	if ( ! class_exists( 'Dashboard_Glancer' ) ) {
+		require plugin_dir_path( __FILE__ ) . 'includes/class-dashboard-glancer.php';  // WP 3.8
+	}
+
+	require plugin_dir_path( __FILE__ ) . 'includes/class-post-type-admin.php';
+
+	$post_type_admin = new SSM_Industries_Admin( $post_type_registrations );
+	$post_type_admin->init();
+
+}
+
+
+add_action( 'admin_enqueue_scripts', 'ssm_industries_admin_enqueue_scripts' );
+
+function ssm_industries_admin_enqueue_scripts() {
+
+  wp_enqueue_style( 'ssm-industries-css', plugins_url('admin.css', __FILE__ ) );
+
+}
+
+require plugin_dir_path( __FILE__ ) . 'includes/plugin_update_check.php';
+
+$MyUpdateChecker = new PluginUpdateChecker_2_0 (
+    'https://kernl.us/api/v1/updates/58404da74201f306a6d77ea7/',
+    __FILE__,
+    'ssm-industries',
+    1
+);
